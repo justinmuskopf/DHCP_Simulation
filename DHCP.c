0ee2bc6395c00427a8ace5b0f0b3d4ed7c8cb534
Justin Muskopf <birmesepython@gmail.com>
2018-04-29 18:11:23 -0500

    Add check for used IP in IP gen

--- DHCP.c
+++ DHCP.c
@@ -4,7 +4,31 @@
 #include <string.h>
 #include "DHCP.h"
 
+#define NUM_STORED 50
 
+typedef enum {FALSE, TRUE} BOOL;
+
+char Used_Ips[NUM_STORED][IP_LEN];
+
+BOOL isValidIp(char *ip, char *gateway)
+{
+	if (!strcmp(ip, gateway))
+		return FALSE;
+		
+	int i;
+	for (i = 0; i < NUM_STORED; i++)
+	{
+		if (!strlen(Used_Ips[i]))
+			break;
+		
+		if (!strcmp(Used_Ips[i], ip))
+			return FALSE;
+	}
+	
+	return TRUE;
+}
+
+//Initialize the first server DHCP packet
 void initSrvDHCP(DHCP_Packet *pkt, char *gateway, char *subnet)
 {
     srand(time(NULL));
@@ -18,6 +42,7 @@ void initSrvDHCP(DHCP_Packet *pkt, char *gateway, char *subnet)
     pkt -> ack = 0;
 }
 
+//Initialize the first client DHCP packet
 void initCliDHCP(DHCP_Packet *pkt)
 {
     srand(time(NULL));
@@ -26,6 +51,7 @@ void initCliDHCP(DHCP_Packet *pkt)
     strcpy(pkt -> yiaddr, "0.0.0.0");
 }
 
+//Generate a random valid IP
 char *generateRandomIP(char *gateway, char *subnet)
 {
     //Get the values of each part
@@ -44,45 +70,48 @@ char *generateRandomIP(char *gateway, char *subnet)
     //Invalid subnet
     if (idx == 4)
     {
-        printf("Invalid Subnet! All IP's occupied.\n");
+        printf("Invalid Subnet! All IPs occupied.\n");
         exit(0);
     }
 
     //Beginning and end of the range
     int begin = gateArr[idx] & subArr[idx];
     int end = (~subArr[idx] & 255) | begin;
-
+	
     //Beginning IP, End IP, Random IP
-    int begIpArr[4];
-    int endIpArr[4];
     int randArr[4];
 
     int i;
     for (i = 0; i < idx; i++)
     {
         randArr[i] = gateArr[i];
-    }
+    }    
 
-    randArr[idx] = (rand() % (end - begin)) + begin;    
-
+	//Create random IP
+    char *randIP = malloc(sizeof(char) * IP_LEN);
+	
+	//Generate random IPs and make sure not already used
     do
     {
+		randArr[idx] = (rand() % (end - begin)) + begin;
+		
         for (i = idx + 1; i < 4; i++)
         {
             randArr[i] = rand() % 255;
         }
-    } while (randArr == gateArr);    
-
-    char *randIP = malloc(sizeof(char) * IP_LEN);
-    sprintf(randIP, "%d.%d.%d.%d", randArr[0], randArr[1], randArr[2], randArr[3]);
+		
+		sprintf(randIP, "%d.%d.%d.%d", randArr[0], randArr[1], randArr[2], randArr[3]);
+		
+    } while (!isValidIp(randIP, gateway);    
+    
     printf("Random IP: %s\n", randIP);
 
     return randIP;
 }
 
+//Generate a random transaction ID
 int generateRandomID()
 {
     //Random ID from 100-999
     return (rand() % 900) + 100;
 }
-
